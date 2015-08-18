8f92399121dba3c906e5c4645d48554b971019a2
Weer <circlesquaredpublishing@gmail.com>
2015-08-18 02:20:29 -0400

    Clean up files.

---
+++ geocoder/index.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Template Name: PHP Geocoder
+Author: Circle Squared Data Labs
+Author URI: http://www.heatery.io
+Description: Geocode input and query by distance.
+Version: 1.0.0
+License: Except as otherwise noted, the content of this page is licensed under the Creative Commons Attribution 3.0 License, and code samples are licensed under the Apache 2.0 License.
+License URI: license.txt
+Tags: responsive-layout, fluid-layout, custom-background, custom-menu, custom-maps, AJAX, Facebook Graph API, Heatmap Visualizations, Google Maps, Bootstrap, Heatery, Circle Squared Data Labs, restaurants, social media analysis, spatial data analysis
+*/
+?>
+
+<?php require 'assets/common/geo_header.php';?>
+
+<?php require 'assets/common/geo_nav.php';?>  
+
+<?php require 'assets/common/geo_form.php';?> 
+
+<?php require 'assets/common/geo_conn.php';?>
+
+<?php require 'assets/common/geo_post.php';?>
+
+<?php require 'assets/common/geo_insert.php';?>
+
+<?php require 'assets/common/geo_table.php';?>
