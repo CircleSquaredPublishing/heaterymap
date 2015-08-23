6c4af7fbb4e6fe8cdd4ec37c3f78798f1d17047b
Weer <circlesquaredpublishing@gmail.com>
2015-08-22 21:50:39 -0400

    Map data insertion

--- markers/assets/common/mrk_insert.php
+++ markers/assets/common/mrk_insert.php
@@ -5,20 +5,32 @@ Description: Inserts Facebook Graph API data into social_data database. Used onl
 Author: Circle Squared Data Labs 
 Author URI: http://www.heatery.io 
 */ 
+
+
 $table = basename(__FILE__, '.php');
-$name = ('assets/common/' . $table . '.json');
-$fp = fopen( $name, 'w' );
-$curl=curl_init('https://graph.facebook.com/v2.4/search?q=restaurant&type=place&distance=8000&center='. $latitude . ',' . $longitude . '&fields=location,name,likes,talking_about_count,were_here_count,description,website&limit=250&access_token=1452021355091002|x-ZB0iKqWQmYqnJQ-wXoUjl-XtY');
-curl_setopt($curl, CURLOPT_URL);
-curl_setopt($curl, CURLOPT_FILE, $fp);
-curl_exec($curl);
-curl_close($curl);
-$results=file_get_contents($name);
-$fb_array=json_decode($results, true);
-foreach ($fb_array[data] as $i) {
-json_decode($i, true);
-echo PHP_EOL;
-//BEGIN QUERY10 
+$name = ('assets/common/data/' . $table . '.json');
+$fp = fopen( $name, 'w' );//Create the file to store the response. Make it writeable.
+
+
+$ch=curl_init();//Initialize the curl function.
+
+curl_setopt($ch, CURLOPT_URL, 'https://graph.facebook.com/v2.4/search?q=restaurant&type=place&distance=8000&center='. $latitude . ',' . $longitude . '&fields=location,name,likes,talking_about_count,were_here_count,description,website&limit=250&access_token=1452021355091002|x-ZB0iKqWQmYqnJQ-wXoUjl-XtY');//Request data from here
+
+
+curl_setopt($ch, CURLOPT_FILE, $fp);//Write to this file. Defined above.
+
+curl_exec($ch);//Execute the command.
+
+curl_close($ch);//Close the command and clear memory.
+
+$results=file_get_contents($name);//Get the contents of the cURL command.
+
+$fb_array=json_decode($results, true);//Decode the JSON string.
+
+foreach ($fb_array[data] as $i) {//Start of the loop that will decode nested values and insert all values into database.
+json_decode($i, true);//Decode the nested values too.
+echo PHP_EOL;//Add newlines to make it readable.
+
 //Insert the data held in the variables into the db
 $stmt1=$conn->prepare("INSERT INTO top10_markers(FID, fb_web,fb_description,fb_name, fb_likes, fb_were_here, fb_talking_about, fb_street, fb_city, fb_state, fb_zip, fb_lat, fb_lng)VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
 $stmt1-> bind_param("dsssiiisssidd", $FID, $fb_web, $fb_description, $fb_name, $fb_likes, $fb_were_here, $fb_talking_about, $fb_street, $fb_city, $fb_state, $fb_zip, $fb_lat, $fb_lng);
