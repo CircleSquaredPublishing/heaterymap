a13b32ea6ac25060724ccbce02dc888a1baaf27c
Weer <circlesquaredpublishing@gmail.com>
2015-08-27 19:57:57 -0400

    massive overhaul to the heatery program

--- kimono/pbc/assets/db/kim_pbc_call.php
+++
@@ -1,5 +0,0 @@
-<?php
-$request = "https://www.kimonolabs.com/api/9ltni8v6?apikey=FBDJYl1bPU9v5loco8ASRjQzBejBLbyW";
-$response = file_get_contents($request);
-$results = json_decode($response, TRUE);
-?>
--- streetview/assets/common/sv_footer.php
+++
@@ -1,5 +0,0 @@
-<?php/*Silence is almost golden...*/?>
-<!--@end initialize function-->
-<script src="/github/streetview/assets/js/sv_main.js"></script>    
-</body><!--@end body & content-->
-</html><!--@end html & page-->
