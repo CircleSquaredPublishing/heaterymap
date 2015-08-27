9b869719cc499e9a08dbb440b18b8d21927b5068
Weer <circlesquaredpublishing@gmail.com>
2015-08-27 19:39:28 -0400

    cleaned up footer

--- heaterymap/hm-assets/hm-common/hm_footer.php
+++ heaterymap/hm-assets/hm-common/hm_footer.php
@@ -1,2 +1,50 @@
 <?php /*Silence is almost golden...*/ ?>
-<script async defer id="github-bjs" src="https://buttons.github.io/buttons.js"></script><script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script', 'twitter-wjs');</script><div id="fb-root"></div><script>(function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(d.getElementById(id))return;js=d.createElement(s);js.id=id;js.src="//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.4&appId=1452021355091002";fjs.parentNode.insertBefore(js,fjs);}(document,'script','facebook-jssdk'));</script></body></html>
+
+<script src="/github/heaterymap/hm-assets/hm-js/hm_theme_selector.js"></script>
+
+<script async defer id="github-bjs" src="https://buttons.github.io/buttons.js"></script>
+
+<script>!function(d,s,id){
+    
+    var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';
+    
+                          if(!d.getElementById(id))
+                              
+                          {
+                              js=d.createElement(s);
+                              
+                              js.id=id;js.src=p+'://platform.twitter.com/widgets.js';
+                              
+                              fjs.parentNode.insertBefore(js,fjs);
+                              
+                          }
+    
+}
+
+(document,'script', 'twitter-wjs');
+    
+</script>
+
+<div id="fb-root"></div>
+
+<script>
+    
+    (function(d,s,id){
+        
+        var js,fjs=d.getElementsByTagName(s)[0];
+        
+        if(d.getElementById(id))return;js=d.createElement(s);
+        
+        js.id=id;js.src="//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.4&appId=1452021355091002";
+        
+        fjs.parentNode.insertBefore(js,fjs);
+        
+    }
+     
+     (document,'script','facebook-jssdk'));
+    
+</script>
+
+</body>
+
+</html>
