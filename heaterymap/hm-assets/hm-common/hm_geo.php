e411994fd94d548c79b0955bec176174858b89b8
Weer <circlesquaredpublishing@gmail.com>
2015-08-27 11:59:51 -0400

    Google geocoder

---
+++ heaterymap/hm-assets/hm-common/hm_geo.php
@@ -0,0 +1,32 @@
+<?php
+if(!$_POST){
+     echo "<img src=\"https://heatery.io/heaterymap/hm-assets/hm-media/hm_static_map.png\"/>";
+}else{
+    $data_arr=geocode($_POST['address']);
+    if($data_arr){
+        $latitude=$data_arr[0];
+        $longitude=$data_arr[1];
+        $city=$data_arr[2];
+    }
+}
+function geocode($address){
+    $address = urlencode($address);
+    $url="https://maps.google.com/maps/api/geocode/json?sensor=false&address={$address}";
+    $resp_json = file_get_contents($url);
+    $resp = json_decode($resp_json, true);
+    if($resp['status']='OK'){
+        $lati=$resp['results'][0]['geometry']['location']['lat'];
+        $longi = $resp['results'][0]['geometry']['location']['lng'];
+        $city = $resp['results'][0]['address_components'][0]['long_name'];
+        if($lati && $longi && $city){
+            $data_arr = array();
+            array_push($data_arr, $lati, $longi, $city);
+            return $data_arr;
+        }else{
+            return false;
+        }
+    }else{
+        return false;
+    }
+}
+?>
