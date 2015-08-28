9b014011ec2121c11631dce947dc38f693476a05
Weer <circlesquaredpublishing@gmail.com>
2015-08-28 14:48:10 -0400

    start page
    set up to match heatery map nav bars

--- hm-pages/hm-start/hm-assets/hm-common/hm_page.php
+++ hm-pages/hm-start/hm-assets/hm-common/hm_page.php
@@ -1,81 +1,188 @@
 <?php /*The main content area for our home page.*/ ?>
-    <nav class="navbar navbar-inverse">
-        <div class="container-fluid">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+    <nav id="hm_navbar_top" class="navbar navbar-default navbar-fixed-top">
+
+        <div id="hm_navbar_container" class="container-fluid">
+
+            <div id="hm_navbar_header" class="navbar-header">
+
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+
+                    <span class="sr-only">Toggle navigation</span>
+
                     <span class="icon-bar"></span>
+
                     <span class="icon-bar"></span>
+
                     <span class="icon-bar"></span>
+
                 </button>
-                <ul class="nav navbar-nav">
-                    <li>
-                        <img style="margin:5px 25px 5px 5px;" width="45" height="45" src="<?php echo $img_path . 'hm_logo_csq_lg.jpg';?>" />
-                    </li>
-                </ul>
-                <a class="navbar-brand" href="https://www.heatery.io/wp-login.php">heatery.io</a>
+
             </div>
-            <div class="collapse navbar-collapse" id="myNavbar">
+            <!--/.hm_navbar_header-->
+
+            <div id="hm_navbar_collapse" class="collapse navbar-collapse">
+
+                <ul class="nav navbar-nav navbar-left">
+
+                    <li>
+                        <button id="btn-sidebar-toggle" type="button" class="btn btn-default navbar-btn">
+                            <span class="glyphicon glyphicon-arrow-left"></span> &nbsp;&nbsp;Close
+                        </button>
+                    </li>
+
+                    <li>
+                        <a href="https://www.heatery.io/login">Login<span class="sr-only">(login)</span></a>
+                    </li>
+
+                    <li>
+                        <a href="http://localhost/github/heaterymap/">Heatery Map<span class="sr-only">(current)</span></a>
+                    </li>
+
+                </ul>
+
                 <ul class="nav navbar-nav navbar-right">
-                    <li>
-                        <a href="https://www.heatery.io/wp-login.php">Login</a>
+
+                    <li class="dropdown">
+
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Connect
+
+<span class="caret"></span>
+
+</a>
+
+                        <ul id="hm_navbar_dropdown_ul" class="dropdown-menu">
+
+                            <li><a href="#">Facebook</a>
+
+                            </li>
+                            <li><a href="#">Twitter</a>
+
+                            </li>
+                            <li><a href="#">Instagram</a>
+
+                            </li>
+                            <li role="separator" class="divider"></li>
+                            <li><a href="#">Data Analysis Portal</a>
+
+                            </li>
+
+                        </ul>
+
                     </li>
-                    <li>
-                        <a href="https://www.heatery.io/wp-login.php?action=register">Register</a>
-                    </li>
-                    <li data-toggle="tooltip" title="Business partners gain access to rich data sets, custom reports, and personalized tools for analytics. All in addition to the this snazzy portal link!">
-                        <a href="https://www.heatery.io/wp-login.php">
-                            <span class="glyphicon glyphicon-user"></span>&nbsp;&nbsp;Client Portal
+
+                    <li id="hm_navbar_dropdown" class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Themes<span class="caret"></span></a>
+
+                        <ul id="hm_navbar_dropdown_ul" class="dropdown-menu">
+
+                            <li>
+
+                                <button id="btn-belgium" class="btn btn-default btn-sm" type="button" onclick="" data-toggle="tooltip" title="Add the Selected Stylesheet">
+
+                                    <span class="glyphicon glyphicon-off"></span>&nbsp;&nbsp;Belgium
+
+                                </button>
+
+                                <li>
+
+                                    <button id="btn-redee" class="btn btn-default btn-sm" type="button" onclick="" data-toggle="tooltip" title="Add the Selected Stylesheet">
+
+                                        <span class="glyphicon glyphicon-off"></span>&nbsp;&nbsp;At the Redee
+
+                                    </button>
+
+                                </li>
+
+                                <li>
+
+                                    <button id="btn-pink" class="btn btn-default btn-sm" type="button" onclick="" data-toggle="tooltip" title="Add the Selected Stylesheet">
+
+                                        <span class="glyphicon glyphicon-off"></span>&nbsp;&nbsp;Soft Pink
+
+                                    </button>
+
+                                </li>
+
+                                <li>
+
+                                    <button id="btn-roots" class="btn btn-default btn-sm" type="button" onclick="" data-toggle="tooltip" title="Add the Selected Stylesheet">
+
+                                        <span class="glyphicon glyphicon-off"></span>&nbsp;&nbsp;Roots
+
+                                    </button>
+
+                                </li>
+
+                                <li>
+
+
+                                </li>
+
+
+                        </ul>
+
+                        </li>
+                        <!--/.dropdown-->
+                        <li>
+                            <a href="#">Circle Squared Data Labs</a>
+                        </li>
+
+                        <a id="hm_navbar_brand" class="navbar-brand" href="https://www.heatery.io">
+
+                            <img id="hm_navbar_brand_img" alt="heatery.io" src="https://www.heatery.io/hm-media/hm-img/hm_logo_csq_lg.jpg">
+
                         </a>
-                    </li>
-                    <li data-toggle="tooltip" title="A Blind Tiger is Coming Soon!">
-                        <a href="https://www.heatery.io/wp-login.php">
-                            <span class="glyphicon glyphicon-log-in"></span>&nbsp;&nbsp;The Speak Easy
-                        </a>
-                    </li>
+
                 </ul>
+
             </div>
+            <!-- /.navbar-collapse -->
+
         </div>
+        <!-- /.container-fluid -->
+
     </nav>
+    <!-- /.navbar-default -->
     <div class="sticky-container">
         <ul class="sticky">
             <li>
                 <a href='https://www.facebook.com/heateryApp'>
-                    <img width="32" height="32" title="Heatery Facebook App" alt="" src="<?php echo $img_path . 'fb1.png';?>" /></a>
+<img width="32" height="32" title="Heatery Facebook App" alt="" src="<?php echo $img_path . 'fb1.png';?>" /></a>
                 <a href='https://www.facebook.com/heateryApp'>
                     <p>Facebook</p>
                 </a>
             </li>
             <li>
                 <a href='https://twitter.com/DelrayInfo'>
-                    <img width="32" height="32" title="Hot Tweets, Hot Eats" alt="" src="<?php echo $img_path . 'tw1.png';?>" /></a>
+<img width="32" height="32" title="Hot Tweets, Hot Eats" alt="" src="<?php echo $img_path . 'tw1.png';?>" /></a>
                 <a href='https://twitter.com/DelrayInfo'>
                     <p>Twitter</p>
                 </a>
             </li>
             <li>
                 <a href='https://github.com/Weer/hm_beta'>
-                    <img width="32" height="32" title="Fork Our Repo" alt="" src="<?php echo $img_path . 'git.png';?>" /></a>
+<img width="32" height="32" title="Fork Our Repo" alt="" src="<?php echo $img_path . 'git.png';?>" /></a>
                 <a href='https://github.com/heatery'>
                     <p>GitHub</p>
                 </a>
             </li>
             <li>
                 <a href='https://instagram.com/heatery/'>
-                    <img width="32" height="32" title="Hot Pics" alt="" src="<?php echo $img_path . 'instagram-32.png';?>" /></a>
+<img width="32" height="32" title="Hot Pics" alt="" src="<?php echo $img_path . 'instagram-32.png';?>" /></a>
                 <a href='https://instagram.com/heatery/'>
                     <p>Instagram</p>
                 </a>
             </li>
             <li>
                 <a href='#'>
-                    <img width="32" height="32" title="" alt="" src="<?php echo $img_path . 'rss1.png';?>" /></a>
+<img width="32" height="32" title="" alt="" src="<?php echo $img_path . 'rss1.png';?>" /></a>
                 <a href='#'>
                     <p>RSS</p>
                 </a>
             </li>
             <li>
                 <a href='https://www.pinterest.com/heateryio/'>
-                    <img width="32" height="32" title="Hot Pins" alt="" src="<?php echo $img_path . 'pin1.png';?>" /></a>
+<img width="32" height="32" title="Hot Pins" alt="" src="<?php echo $img_path . 'pin1.png';?>" /></a>
                 <a href='https://www.pinterest.com/heateryio/'>
                     <p>Pinterest</p>
                 </a>
@@ -133,17 +240,133 @@ border-top: 1px solid #989898;">
                                 </li>
                                 <!--@end social media buttons-->
                                 <li style="color:#989898;">
-                                    <p>Crafted with<span style="margin-left:-15px; margin-right:-7px; color:#F2BDB9" class="glyphicon glyphicon-heart"></span>&nbsp;&nbsp; by heatery.io</p>
+                                    <p id="hm_love">Crafted with<span style="margin-left:-15px; margin-right:-7px; color:#F2BDB9" class="glyphicon glyphicon-heart"></span>&nbsp;&nbsp; by heatery.io</p>
                                 </li>
                             </ul>
                         </div>
                         <!-- /#sidebar-wrapper -->
-                        <div id="map-canvas"></div>
+                        <div id="map-canvas" style="top:0px;"></div>
                         <!--@end #map-canvas-->
-                        <a href="#menu-toggle" class="btn btn-default btn-xs" id="menu-toggle"><span class="glyphicon glyphicon-arrow-left"></span>&nbsp;&nbsp;Close</a>
+
                     </div>
                 </div>
             </div>
         </div>
         <!-- /#page-content-wrapper -->
     </div>
+    <nav id="hm_navbar_bottom" class="navbar navbar-default navbar-fixed-bottom">
+
+        <div id="hm_navbar_container" class="container-fluid">
+
+            <div id="hm_navbar_header" class="navbar-header">
+
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+
+                    <span class="sr-only">Toggle navigation</span>
+
+                    <span class="icon-bar"></span>
+
+                    <span class="icon-bar"></span>
+
+                    <span class="icon-bar"></span>
+
+                </button>
+
+            </div>
+            <!--/.hm_navbar_header-->
+
+            <div id="hm_navbar_collapse" class="collapse navbar-collapse">
+                <ul class="nav navbar-nav nabvar-left">
+                    <li>
+
+                        <a href="#">
+
+                            <span class="glyphicon glyphicon-user">
+
+</span>&nbsp;&nbsp;Client Portal
+
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#">
+
+                            <span class="glyphicon glyphicon-log-in">
+
+</span>&nbsp;&nbsp;The Speak Easy&nbsp;&nbsp;&nbsp;&nbsp;</a>
+
+                    </li>
+
+                    
+                </ul>
+
+                <ul class="nav navbar-nav navbar-right">
+                    
+                                        
+                                       <li>
+                        <a href="#">About</a>
+                    </li>
+
+                    <li id="hm_navbar_dropdown" class="dropup">
+
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Developers
+
+<span class="caret"></span>
+
+</a>
+
+                        <ul id="hm_dropup_dev" class="dropdown-menu">
+
+                            <li><a href="#">Github Repos</a>
+
+                            </li>
+
+                            <li><a href="#">Contribute Data</a>
+
+                            </li>
+                            <li><a href="#">Create Art with Us</a>
+
+                            </li>
+                            <li role="separator" class="divider"></li>
+                            <li><a href="#">About our Company</a>
+
+                            </li>
+
+                        </ul>
+
+                    </li>
+
+                    <li id="hm_navbar_dropdown" class="dropup">
+
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Blog<span class="caret"></span>
+</a>
+
+                        <ul id="hm_dropup_blog" class="dropdown-menu">
+
+                            <li>
+                                <a href="#">Articles</a>
+                            </li>
+
+                            <li>
+                                <a href="#">Tutorials</a>
+                            </li>
+
+                            <li>
+                                <a href="#">Raw Data Sets</a>
+                            </li>
+
+                        </ul>
+
+                    </li>
+                    <!--/.dropup-->
+
+                </ul>
+
+            </div>
+            <!-- /.navbar-collapse -->
+
+        </div>
+        <!-- /.container-fluid -->
+
+    </nav>
+    <!-- /.navbar-default -->
