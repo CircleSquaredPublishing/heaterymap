f59db21deccc50ed8d4db9a5b4b1d7da6b854baf
Weer <circlesquaredpublishing@gmail.com>
2015-09-10 13:51:45 -0400

    Addition for Version 2.0.0 which adds Foursquare data to the calculations.

--- heaterymap/hm-assets/hm-common/hm_geo.php
+++ heaterymap/hm-assets/hm-common/hm_geo.php
@@ -1,16 +1,44 @@
 <?php
-if(!$_POST){?>
-<h4><div class="alert alert-success" role="alert">&nbsp;&nbsp;Enter city or address&nbsp;&nbsp;<span class="glyphicon glyphicon-chevron-up"></span>&nbsp;&nbsp;here to get started.</div></h4>
+if(!$_POST){
+?>
+<h4>
+    <div class="alert alert-success" role="alert">
+        &nbsp;&nbsp;Enter city or address&nbsp;&nbsp;
+        <span class="glyphicon glyphicon-chevron-up"></span>
+        &nbsp;&nbsp;here to get started.
+    </div>
+</h4>
 
 <?php
-}else{
-    $data_arr=geocode($_POST['address']);
+} else {
+    
+$data_arr=geocode($_POST['address']);
+    
+    if (!$limit=($_POST['limit'])){
+        $limit=10;
+    }
+    
+    if (!$distance=($_POST['distance'])){
+        $distance=10;
+    }
+    
     if($data_arr){
-        $latitude=$data_arr[0];
-        $longitude=$data_arr[1];
+        
+        $latitude=number_format($data_arr[0], 2);
+        $longitude=number_format($data_arr[1], 2);
         $city=$data_arr[2];
+        /*@ begin foursquare param*/
+        
+        date_default_timezone_set("America/New_York");
+        $today=date('l, F jS');
+        $client_id = "LUDUFON05OQ3US4C0FT0TEKWXKSD0NHIPVGKF0TGUZGY4YUR";
+        $client_secret ="F2E3NQTQKY3WS1APVGFVA31ESHW2ONNPVNJ11NPYVBV05W2I";
+        $version = date('Ymd');
+        /*@ end foursquare param*/
+        
     }
 }
+
 function geocode($address){
     $address = urlencode($address);
     $url="https://maps.google.com/maps/api/geocode/json?sensor=false&address={$address}";
@@ -32,4 +60,3 @@ function geocode($address){
     }
 }
 ?>
-
