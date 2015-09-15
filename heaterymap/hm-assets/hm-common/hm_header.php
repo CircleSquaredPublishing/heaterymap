b4e5f5a621a79a93c31af38000feaf16b8df795b
Circle Squared Publishing <circlesquaredpublishing@gmail.com>
2015-09-15 07:20:25 -0400

    update header

--- heaterymap/hm-assets/hm-common/hm_header.php
+++ heaterymap/hm-assets/hm-common/hm_header.php
@@ -33,24 +33,22 @@ if(isset($pg_title) && is_string($pg_title)){
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="author" content="Circle Squared Data Labs" />
-<!-- GOOGLE ANALYTICS TRACKING CODE -->
+
+<!-- DETECT & REDIRECT -->
+    
 <script>
-    (function(i, s, o, g, r, a, m) {
-        i['GoogleAnalyticsObject'] = r;
-        i[r] = i[r] || function() {
-            (i[r].q = i[r].q || []).push(arguments)
-        }, i[r].l = 1 * new Date();
-        a = s.createElement(o),
-            m = s.getElementsByTagName(o)[0];
-        a.async = 1;
-        a.src = g;
-        m.parentNode.insertBefore(a, m)
-    })(window, document, 'script',
-        '//www.google-analytics.com/analytics.js', 'ga');
-    ga('create', 'UA-64702784-1', 'auto');
-    ga('send', 'pageview');
-</script>
+    
+    if (screen.width <=699) {
+        
+        document.location = "https://www.mobile.heatery.io";
+        
+        }
+    
+</script>   
+    
+
 <!-- - - - - - - - - - - EXTERNAL ASSETS - - - - - - - - - - -->
+    
 <!-- JQUERY 2.1.4 -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     
