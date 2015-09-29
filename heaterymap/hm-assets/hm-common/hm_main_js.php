e43e78eb292245836448bd0f8706f99fffd27918
Weer <circlesquaredpublishing@gmail.com>
2015-09-29 13:41:08 -0400

    main javascript file
    Added Mercator projection functions that recalculate pixel size of heat
    map layer when zooming.

--- heaterymap/hm-assets/hm-common/hm_main_js.php
+++ heaterymap/hm-assets/hm-common/hm_main_js.php
@@ -2,26 +2,81 @@
     var map;
     var marker;
     var infoWindow;
-
+    var myData=[];
+    var TILE_SIZE = 256;
+    /*Begin Mercator Projection*/
+function bound(value, opt_min, opt_max) {
+	if (opt_min !== null) value = Math.max(value, opt_min);
+	if (opt_max !== null) value = Math.min(value, opt_max);
+	return value;
+}
+function degreesToRadians(deg) {
+	return deg * (Math.PI / 180);
+}
+function radiansToDegrees(rad) {
+	return rad / (Math.PI / 180);
+}
+function MercatorProjection() {
+	this.pixelOrigin_ = new google.maps.Point(TILE_SIZE / 2, TILE_SIZE / 2);
+	this.pixelsPerLonDegree_ = TILE_SIZE / 360;
+	this.pixelsPerLonRadian_ = TILE_SIZE / (2 * Math.PI);
+}
+MercatorProjection.prototype.fromLatLngToPoint = function(latLng, opt_point) {
+	var me = this;
+	var point = opt_point || new google.maps.Point(0, 0);
+	var origin = me.pixelOrigin_;
+	point.x = origin.x + latLng.lng() * me.pixelsPerLonDegree_;
+	var siny = bound(Math.sin(degreesToRadians(latLng.lat())), -0.9999, 0.9999);
+	point.y = origin.y + 0.5 * Math.log((1 + siny) / (1 - siny)) * -me.pixelsPerLonRadian_;
+	return point;
+};
+MercatorProjection.prototype.fromPointToLatLng = function(point) {
+	var me = this;
+	var origin = me.pixelOrigin_;
+	var lng = (point.x - origin.x) / me.pixelsPerLonDegree_;
+	var latRadians = (point.y - origin.y) / -me.pixelsPerLonRadian_;
+	var lat = radiansToDegrees(2 * Math.atan(Math.exp(latRadians)) - Math.PI / 2);
+	return new google.maps.LatLng(lat, lng);
+};
+var desiredRadiusPerPointInMeters = 150;
+function getNewRadius() {
+	var numTiles = 1 << map.getZoom();
+	var center = map.getCenter();
+	var moved = google.maps.geometry.spherical.computeOffset(center, 10000, 90); /*1000 meters to the right*/
+	var projection = new MercatorProjection();
+	var initCoord = projection.fromLatLngToPoint(center);
+	var endCoord = projection.fromLatLngToPoint(moved);
+	var initPoint = new google.maps.Point(
+		initCoord.x * numTiles,
+		initCoord.y * numTiles);
+	var endPoint = new google.maps.Point(
+		endCoord.x * numTiles,
+		endCoord.y * numTiles);
+	var pixelsPerMeter = (Math.abs(initPoint.x - endPoint.x)) / 10000.0;
+	var totalPixelSize = Math.floor(desiredRadiusPerPointInMeters * pixelsPerMeter);
+	console.log(totalPixelSize);
+	return totalPixelSize;
+}
+/*End Mercator Projection*/
     function displayMap() {
         var retro_style = new google.maps.StyledMapType(retroStyle, {
             name: "Retro"
-        });
+});
         var apple_style = new google.maps.StyledMapType(appleStyle, {
             name: "Apple"
-        });
+});
         var light_style = new google.maps.StyledMapType(lightStyle, {
             name: "Dusk"
-        });
+});
         var old_style = new google.maps.StyledMapType(oldStyle, {
             name: "Vintage"
-        });
+});
         var pale_style = new google.maps.StyledMapType(paleStyle, {
             name: "Cloud"
-        });
+});
         var brown_style = new google.maps.StyledMapType(brownStyle, {
             name: "Organic"
-        });
+});
         var myOptions = {
             zoom: 14,
             center: new google.maps.LatLng(<?php echo $latitude; ?>, <?php echo $longitude;?>),
@@ -46,8 +101,15 @@
         map.mapTypes.set("Cloud", pale_style);
         map.mapTypes.set("Organic", brown_style);
         map.setMapTypeId("Retro");
+        
+	google.maps.event.addListener(map, 'zoom_changed', function() {
+		heatmap.setOptions({
+			radius: getNewRadius()
+		});
+	});
         <?php require ($common_path . 'hm_add.php');?>
-        }
+
+    }
 
     $(document).ready(function() {
         $.ajax({
@@ -70,7 +132,7 @@
                 }
                 heatmap = new google.maps.visualization.HeatmapLayer({
                     data: myData,
-                    radius: 30,
+                    radius: getNewRadius(),
                     opacity: 0.3,
                     gradient: gradientNew,
                     map: map
