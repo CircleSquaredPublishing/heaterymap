051849132ddfa041f6800335c13d7f17228cb6cb
Weer <circlesquaredpublishing@gmail.com>
2015-09-30 23:09:37 -0400

    inserts into database: mobile version

--- mobileHeatery/heaterymap/hm-assets/hm-common/hm_insert.php
+++ mobileHeatery/heaterymap/hm-assets/hm-common/hm_insert.php
@@ -26,7 +26,7 @@ $table = basename(__FILE__ , '.php');
 $name = ($table . '.json');
 $fp = fopen($name, 'w' );
 $ch=curl_init();
-curl_setopt($ch, CURLOPT_URL, 'https://graph.facebook.com/v2.4/search?q=restaurant&type=place&distance=8000&center='. $latitude . ',' . $longitude . '&fields=location,name,likes,talking_about_count,were_here_count,description,website,cover,about,culinary_team&limit=250&access_token=1452021355091002|x-ZB0iKqWQmYqnJQ-wXoUjl-XtY');
+curl_setopt($ch, CURLOPT_URL, 'https://graph.facebook.com/v2.4/search?q=restaurant&type=place&distance=3200&center='. $latitude . ',' . $longitude . '&fields=location,name,likes,talking_about_count,were_here_count,description,website,cover,about,culinary_team&limit=250&access_token=1452021355091002|x-ZB0iKqWQmYqnJQ-wXoUjl-XtY');
 curl_setopt($ch, CURLOPT_FILE, $fp);
 curl_exec($ch);
 curl_close($ch);
@@ -61,7 +61,11 @@ $stmt1->close();
 $table_fs=basename(__FILE__,'.php');
 $name_fs=($table_fs .'foursquare'.'.json');
 $fp_fs=fopen($name_fs,'w');
-$fs_url="https://api.foursquare.com/v2/venues/search?ll=" . $latitude . "," . $longitude . "&limit=50&radius=2000&categoryId=4d4b7105d754a06374d81259&client_id=$client_id&client_secret=$client_secret&v=$version";
+
+
+$fs_url="https://api.foursquare.com/v2/venues/explore?v=$version&ll=".$latitude.",".$longitude."&radius=3200&section=food&limit=50&novelty=new&client_id=$client_id&client_secret=$client_secret";
+
+
 $ch_fs=curl_init();
 curl_setopt($ch_fs,CURLOPT_URL,$fs_url);
 curl_setopt($ch_fs,CURLOPT_FILE,$fp_fs);
@@ -70,53 +74,130 @@ curl_close($ch_fs);
 $results_fs=file_get_contents($name_fs);
 $response_fs=json_decode($results_fs,true);
 
-foreach($response_fs[response][venues] as $fs){
+foreach($response_fs['response']['groups'] as $v){    
+foreach($v['items'] as $v2){
+foreach($v2['venue']['categories'] as $v3){      
+foreach($v2['tips'] as $v4){ 
+
+$stmt_fs= $conn->prepare(
+    "INSERT INTO foursquare_explore(
+    `fsid`,                  
+    `fs_name`,               
+    `fs_phone`,              
+    `fs_address`,            
+    `fs_lat`,                
+    `fs_lng`,                
+    `fs_postalCode`,         
+    `fs_cc`,                 
+    `fs_city`,               
+    `fs_state`,              
+    `fs_country`,            
+    `formattedAddress1`,     
+    `formattedAddress2`,     
+    `formattedAddress3`,     
+    `fs_checkins`,           
+    `fs_users`,              
+    `fs_tips`,               
+    `fs_url`,               
+    `fs_price_msg`,         
+    `fs_price_sym`,          
+    `fs_rating`,             
+    `fs_rating_signals`,     
+    `fs_hours`,              
+    `fs_hereNow`,            
+    `fs_category`,           
+    `fs_tips_created`,       
+    `fs_tips_text`,          
+    `fs_tips_url`,           
+    `fs_tips_likes`,         
+    `fs_tips_userId`,        
+    `fs_tips_userFirst`,     
+    `fs_tips_userLast`,      
+    `fs_tips_userPicPre`,    
+    `fs_tips_userPicSuff`)
+    VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
     
-    $stmt_fs= $conn->prepare(
-        "INSERT INTO foursquare(
-        FSID, 
-        fs_name, 
-        fs_phone,  
-        fs_reservations,
-        fs_menu, 
-        fs_mobile_menu, 
-        fs_tips, 
-        fs_checkins, 
-        fs_users, 
-        fs_city, 
-        fs_lat, 
-        fs_lng)
-        VALUES
-        (?,?,?,?,?,?,?,?,?,?,?,?)"); 
+$stmt_fs->bind_param("ssssddisssssssiiisssdisissssiissss",
+                  
+    $fsid,                  
+    $fs_name,               
+    $fs_phone,              
+    $fs_address,            
+    $fs_lat,                
+    $fs_lng,                
+    $fs_postalCode,         
+    $fs_cc,                 
+    $fs_city,               
+    $fs_state,              
+    $fs_country,            
+    $formattedAddress1,     
+    $formattedAddress2,     
+    $formattedAddress3,     
+    $fs_checkins,           
+    $fs_users,              
+    $fs_tips,               
+    $fs_url,                
+    $fs_price_msg,         
+    $fs_price_sym,          
+    $fs_rating,             
+    $fs_rating_signals,     
+    $fs_hours,              
+    $fs_hereNow,            
+    $fs_category,           
+    $fs_tips_created,       
+    $fs_tips_text,          
+    $fs_tips_url,           
+    $fs_tips_likes,         
+    $fs_tips_userId,        
+    $fs_tips_userFirst,     
+    $fs_tips_userLast,      
+    $fs_tips_userPicPre,    
+    $fs_tips_userPicSuff);
+               
     
-    $stmt_fs->bind_param("ssssssiiisdd", 
-                       $FSID,
-                       $fs_name,
-                       $fs_phone, 
-                       $fs_reservations,
-                       $fs_menu, 
-                       $fs_mobile_menu, 
-                       $fs_tips, 
-                       $fs_checkins, 
-                       $fs_users, 
-                       $fs_city, 
-                       $fs_lat, 
-                       $fs_lng);
-    
-    $FSID = mysqli_real_escape_string($conn, $fs['id']); 
-    $fs_name = mysqli_real_escape_string($conn, $fs['name']);
-    $fs_phone= mysqli_real_escape_string($conn, $fs['contact']['formattedPhone']);
-    $fs_reservations= mysqli_real_escape_string($conn, $fs['reservations']['url']); 
-    $fs_menu= mysqli_real_escape_string($conn, $fs['menu']['url']);    
-    $fs_mobile_menu= mysqli_real_escape_string($conn, $fs['menu']['mobileUrl']);   
-    $fs_tips = mysqli_real_escape_string($conn, $fs['stats']['tipCount']);   
-    $fs_checkins = mysqli_real_escape_string($conn, $fs['stats']['checkinsCount']);    
-    $fs_users = mysqli_real_escape_string($conn, $fs['stats']['usersCount']); 
-    $fs_city = mysqli_real_escape_string($conn, $fs['location']['city']); 
-    $fs_lat = mysqli_real_escape_string($conn, $fs['location']['lat']);    
-    $fs_lng = mysqli_real_escape_string($conn, $fs['location']['lng']);    
-    $stmt_fs -> execute();
-    } 
+    $fsid=                  mysqli_real_escape_string($conn,$v2['venue']['id']);                 
+    $fs_name=               mysqli_real_escape_string($conn,$v2['venue']['name']);               
+    $fs_phone=              mysqli_real_escape_string($conn,$v2['venue']['contact']['formattedPhone']);              
+    $fs_address=            mysqli_real_escape_string($conn,$v2['venue']['location']['address']);            
+    $fs_lat=                mysqli_real_escape_string($conn,$v2['venue']['location']['lat']);                
+    $fs_lng=                mysqli_real_escape_string($conn,$v2['venue']['location']['lng']);                
+    $fs_postalCode=         mysqli_real_escape_string($conn,$v2['venue']['location']['postalCode']);         
+    $fs_cc=                 mysqli_real_escape_string($conn,$v2['venue']['location']['cc']);                 
+    $fs_city=               mysqli_real_escape_string($conn,$v2['venue']['location']['city']);               
+    $fs_state=              mysqli_real_escape_string($conn,$v2['venue']['location']['state']);              
+    $fs_country=            mysqli_real_escape_string($conn,$v2['venue']['location']['country']);            
+    $formattedAddress1=     mysqli_real_escape_string($conn,$v2['venue']['location']['formattedAddress'][0]);     
+    $formattedAddress2=     mysqli_real_escape_string($conn,$v2['venue']['location']['formattedAddress'][1]);     
+    $formattedAddress3=     mysqli_real_escape_string($conn,$v2['venue']['location']['formattedAddress'][2]);     
+    $fs_checkins=           mysqli_real_escape_string($conn,$v2['venue']['stats']['checkinsCount']);           
+    $fs_users=              mysqli_real_escape_string($conn,$v2['venue']['stats']['usersCount']);              
+    $fs_tips=               mysqli_real_escape_string($conn,$v2['venue']['stats']['tipCount']);               
+    $fs_url=                mysqli_real_escape_string($conn,$v2['venue']['url']);                
+    $fs_price_msg=          mysqli_real_escape_string($conn,$v2['venue']['price']['message']);         
+    $fs_price_sym=          mysqli_real_escape_string($conn,$v2['venue']['price']['currency']);          
+    $fs_rating=             mysqli_real_escape_string($conn,$v2['venue']['rating']);             
+    $fs_rating_signals=     mysqli_real_escape_string($conn,$v2['venue']['ratingSignals']);     
+    $fs_hours=              mysqli_real_escape_string($conn,$v2['venue']['hours']['isOpen']);              
+    $fs_hereNow=            mysqli_real_escape_string($conn,$v2['venue']['hereNow']['count']);            
+    $fs_category=           mysqli_real_escape_string($conn,$v3['shortName']);           
+    $fs_tips_created=       mysqli_real_escape_string($conn,$v4['createdAt']);       
+    $fs_tips_text=          mysqli_real_escape_string($conn,$v4['text']);          
+    $fs_tips_url=           mysqli_real_escape_string($conn,$v4['canonicalUrl']);           
+    $fs_tips_likes=         mysqli_real_escape_string($conn,$v4['likes']['count']);         
+    $fs_tips_userId=        mysqli_real_escape_string($conn,$v4['user']['id']);        
+    $fs_tips_userFirst=     mysqli_real_escape_string($conn,$v4['user']['firstName']);     
+    $fs_tips_userLast=      mysqli_real_escape_string($conn,$v4['user']['lastName']);      
+    $fs_tips_userPicPre=    mysqli_real_escape_string($conn,$v4['user']['photo']['prefix']);    
+    $fs_tips_userPicSuff=   mysqli_real_escape_string($conn,$v4['user']['photo']['suffix']);
+          
+    $stmt_fs->execute();
+            }
+        
+        }
+
+    }
+
+}
     
 $stmt_fs->close();
 ?>
