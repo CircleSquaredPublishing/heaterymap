7acf39ff9d82140ff57ff0cbb95c85b2aeab93ba
Weer <circlesquaredpublishing@gmail.com>
2015-09-30 23:07:44 -0400

    adds data to map

--- heaterymap/hm-assets/hm-common/hm_add.php
+++ heaterymap/hm-assets/hm-common/hm_add.php
@@ -88,196 +88,160 @@ foreach($heatery_score as $x){
 }
 
 for ($i = 0; $i < $c; $i++) {
-
 $icons= array("number_1.png", "number_2.png", "number_3.png", "number_4.png" ,"number_5.png", "number_6.png", "number_7.png", "number_8.png", "number_9.png", "number_10.png");
-
     $arrlength=count($icons);
-    
-        for ($i = 0;$i<$arrlength;$i++) {
+for ($i = 0;$i<$arrlength;$i++) {
+    $j = $i + 1; 
+    echo 
+        "var bounds$i = new google.maps.LatLngBounds(); \n";
+    echo 
+        "var point$i = new google.maps.LatLng(\"$fb_lat[$i]\",\"$fb_lng[$i]\"); \n";
+    echo 
+        "var marker$i = new google.maps.Marker({
+        position: point$i,   
+        map: map,  
+        icon: '//heatery.io/heaterymap/hm-assets/hm-media/hm-markers-colors-000000/$icons[$i]'
+        });\n";
+    echo
+        "bounds$i.extend(marker$i.getPosition());\n";
+    echo
+        "var fb_date$i = \"$fb_date[$i]\"; \n";
+    echo
+        "var fb_name$i = \"$fb_name[$i]\"; \n";
+    echo
+        "var fb_street$i = \"$fb_street[$i]\";\n"; 
+    echo
+        "var fb_city$i = \"$fb_city[$i]\";\n"; 
+    echo
+        "var fb_state$i = \"$fb_state[$i]\";\n";   
+    echo
+        "var fb_zip$i = \"$fb_zip[$i]\";\n";
+    echo
+        "var fb_talking_about$i = \"$fb_talking_about[$i]\";\n";
+    echo
+        "var fb_likes$i = \"$fb_likes[$i]\";\n";
+    echo
+        "var fb_were_here$i = \"$fb_were_here[$i]\";\n";
+    echo
+        "var fb_description$i = \"{$fb_description[$i]}\";\n";
+    echo
+        "var fb_web$i = \"$fb_web[$i]\";\n";       
+    echo
+        "var fb_web_parse$i = \"$fb_web_parse[$i]\";\n";       
+    echo
+        "var fb_cover$i = \"$fb_cover[$i]\";\n";       
+    echo
+        "var fb_about$i = \"$fb_about[$i]\";\n";        
+    echo
+        "var fb_culinary_team$i = \"{$fb_culinary_team[$i]}\";\n";           
+    echo
+        "var fs_name$i = \"$fs_name[$i]\"; \n";            
+    echo
+        "var fs_phone$i = \"$fs_phone[$i]\"; \n";           
+    echo 
+        "var fs_checkins$i = \"$fs_checkins[$i]\";\n";            
+    echo 
+        "var fs_users$i = \"$fs_users[$i]\";\n";           
+    echo 
+        "var fs_tips$i = \"$fs_tips[$i]\";\n";       
+    echo 
+        "var heatery_score$i = \"$heatery_score[$i]\";\n";
+    echo 
+        "var v$i= \"$v[$i]\";\n";
 
-            $j = $i + 1; 
-            echo "var bounds$i = new google.maps.LatLngBounds(); \n";
-
-            echo "var point$i = new google.maps.LatLng(\"$fb_lat[$i]\",\"$fb_lng[$i]\"); \n";
-
-            echo "var marker$i = new google.maps.Marker({
-                position: point$i,   
-                map: map,  
-                icon: '//heatery.io/heaterymap/hm-assets/hm-media/hm-markers-colors-000000/$icons[$i]'
-                });\n";
-            
-            echo "bounds$i.extend(marker$i.getPosition());\n";
-
-            echo "var fb_date$i = \"$fb_date[$i]\"; \n";
-
-            echo "var fb_name$i = \"$fb_name[$i]\"; \n";
-
-            echo "var fb_street$i = \"$fb_street[$i]\";\n";
-            
-            echo "var fb_city$i = \"$fb_city[$i]\";\n";
-            
-            echo "var fb_state$i = \"$fb_state[$i]\";\n";
-            
-            echo "var fb_zip$i = \"$fb_zip[$i]\";\n";
-
-            echo "var fb_talking_about$i = \"$fb_talking_about[$i]\";\n";
-
-            echo "var fb_likes$i = \"$fb_likes[$i]\";\n";
-
-            echo "var fb_were_here$i = \"$fb_were_here[$i]\";\n";
-
-            echo "var fb_description$i = \"{$fb_description[$i]}\";\n";
-
-            echo "var fb_web$i = \"$fb_web[$i]\";\n";
-            
-            echo "var fb_web_parse$i = \"$fb_web_parse[$i]\";\n";
-            
-            echo "var fb_cover$i = \"$fb_cover[$i]\";\n";
-            
-            echo "var fb_about$i = \"$fb_about[$i]\";\n";
-            
-            echo "var fb_culinary_team$i = \"{$fb_culinary_team[$i]}\";\n";
-            
-            echo "var fs_name$i = \"$fs_name[$i]\"; \n";
-            
-            echo "var fs_phone$i = \"$fs_phone[$i]\"; \n";
-            
-            echo "var fs_checkins$i = \"$fs_checkins[$i]\";\n";
-            
-            echo "var fs_users$i = \"$fs_users[$i]\";\n";
-            
-            echo "var fs_tips$i = \"$fs_tips[$i]\";\n";
-            
-            echo "var heatery_score$i = \"$heatery_score[$i]\";\n";
-            
-            echo "var v$i= \"$v[$i]\";\n";
-
-/*Begin Facebook Data*/            
-
+           
 echo "var html$i= 
-
 '<div id=\"iw_container\" class=\"container-fluid\">' +
-
-'<div class=\"row\"><div class=\"col-xs-12\">' + 
-
-'<div id=\"iw_title\">' + 
-
-\"$fb_name[$i]\" +  '&nbsp;ranks higher than&nbsp;' + \"$v[$i]\" + '&nbsp;of qualifying restaurants within the search area.' +
-
-'<br>' +
-
-'</div></div></div><br>' + 
-
-'<div class=\"row\"><div class=\"col-xs-12\">' + 
-
-'</div></div><hr>' + 
-
+\"$j.&nbsp;$fb_name[$i]\" + '<br>' +
+\"$fb_street[$i]\" + '<br>' +
+\"$fs_phone[$i]\" + '&nbsp;&nbsp;' + '<span class=\"glyphicon glyphicon-earphone\"></span>'+
+'</div></div>' + 
 '</div></div></div></div>'\n";  
 
 
-echo  "var infoCard$i = 
+    echo  
+        "var infoCard$i = 
+        '<div class=\"container-fluid\"><div class=\"row\">' + 
+        '<div id=\"sb-title\" class=\"col-xs-12\">' + 
+        \"$j.&nbsp;$fb_name[$i]\" + 
+        '<br>' +  
+        \"$fb_street[$i]\" + 
+        '<br>' + 
+        \"$fs_phone[$i]\" + 
+        '</div><hr>' + 
+        '<a id=\"sb_link\" href=' + \"$fb_web[$i]\" + '>' + \"$fb_web_parse[$i]\" +  '</a>' + 
+        '<br>' + 
+        \"$fb_culinary_team[$i]\" + 
+        '<br>' + 
+        '<div class=\"row\"><div class=\"col-xs-12\">' + 
+        '<img id=\"fb_cover\" src=\"$fb_cover[$i]\"/>' + 
+        '</div></div>' + '<hr>' + 
+        '<div class=\"row\"><div class=\"col-xs-12\"><div id=\"sb-content\">' + 
+        '<p>' + \"$fb_about[$i]\" + '<hr>' + \"$fb_description[$i]\" + '</p>' + 
+        '</div></div></div>'; \n";
 
-'<div class=\"container-fluid\"><div class=\"row\">' + 
+    echo 
+        "$('#info_card').append(infoCard$i); \n";
 
-'<div id=\"sb-title\" class=\"col-xs-12\">' + 
+    echo 
+        "var infowindow$i = new google.maps.InfoWindow({
+                content: html$i,
+                maxWidth:350
+                }); \n";
 
-\"$j.&nbsp;$fb_name[$i]\" + 
+    echo 
+        "google.maps.event.addListener(marker$i, 'click', function (){
+                infowindow$i.setContent(html$i);
+                infowindow$i.open(map, marker$i);
+                });\n";
 
-'<br>' +  
-\"$fb_street[$i]\" + 
+    echo 
+        "google.maps.event.addListener(map, 'click', function (){
+                infowindow$i.close();
+                });\n";
 
-'<br>' + 
-\"$fs_phone[$i]\" + 
+    echo 
+        "google.maps.event.addListener(infowindow$i, 'domready', function (){
 
-'</div><hr>' + 
-'<a id=\"sb_link\" href=' + \"$fb_web[$i]\" + '>' + \"$fb_web_parse[$i]\" +  '</a>' + 
+        var iwOuter = $('.gm-style-iw');
+        var iwBackground = iwOuter.prev();
 
-'<br>' + 
-\"$fb_culinary_team[$i]\" + 
+        iwBackground.children(':nth-child(2)').css({
+            'display' : 'none'
+            });
 
-'<br>' + 
+        iwBackground.children(':nth-child(4)').css({
+            'display' : 'none'
+            });
 
-'<div class=\"row\"><div class=\"col-xs-12\">' + 
+        iwBackground.children(':nth-child(3)').find('div').children().css({
+            'box-shadow': 'rgba(82, 66, 4, 0.5); 0px 1px 6px', 
+            'z-index' : '1'
+            });
 
-'<img id=\"fb_cover\" src=\"$fb_cover[$i]\"/>' + 
+        var iwCloseBtn = iwOuter.next();
 
-'</div></div>' + '<hr>' + 
+        iwCloseBtn.css({
+            opacity: '1', 
+            right: '38px', 
+            top: '3px',
+            'border-radius': '13px', 
+            'box-shadow': '0 0 5px rgb(82, 66, 4)'
+            });
 
-'<div class=\"row\"><div class=\"col-xs-12\"><div id=\"sb-content\">' + 
+        if($('.iw-content').height() < 140){
+            $('.iw-bottom-gradient').css({
+            display: 'none'
+            });
+        }
 
-'<p>' + \"$fb_about[$i]\" + '<hr>' + \"$fb_description[$i]\" + '</p>' + 
-
-'</div></div></div>'; \n";
-
-
-echo "$('#info_card').append(infoCard$i); \n";
-
-
-echo "var infowindow$i = new google.maps.InfoWindow({
-    content: html$i,
-    maxWidth:350
-}); \n";
-
-echo "google.maps.event.addListener(marker$i, 'click', function (){
-            infowindow$i.setContent(html$i);
-            infowindow$i.open(map, marker$i);
-             });\n";
-
-echo "google.maps.event.addListener(map, 'click', function (){
-            infowindow$i.close();
-             });\n";
-
-echo "google.maps.event.addListener(infowindow$i, 'domready', function (){
-
-var iwOuter = $('.gm-style-iw');
-
-var iwBackground = iwOuter.prev();
-
-    iwBackground.children(':nth-child(2)').css({
-        'display' : 'none'
-        });
-
-    iwBackground.children(':nth-child(4)').css({
-        'display' : 'none'
-        });
-
-    iwBackground.children(':nth-child(3)').find('div').children().css({
-        'box-shadow': 'rgba(82, 66, 4, 0.5); 0px 1px 6px', 
-        'z-index' : '1'
-        });
-
-var iwCloseBtn = iwOuter.next();
-
-    iwCloseBtn.css({
-        opacity: '1', 
-        right: '38px', 
-        top: '3px',
-        'border-radius': '13px', 
-        'box-shadow': '0 0 5px rgb(82, 66, 4)'
-    });
-
-    if($('.iw-content').height() < 140){
-
-        $('.iw-bottom-gradient').css({
-        display: 'none'
-        });
-
+        iwCloseBtn.mouseout(function(){
+                $(this).css({
+                opacity: '1'
+                });
+            });
+        });\n";        
     }
-
-    iwCloseBtn.mouseout(function(){
-
-        $(this).css({
-        opacity: '1'
-        });
-
-    });
-
-});\n";
-            
+    echo "map.fitBounds(bounds$i); \n"; 
 }
-
-    echo "map.fitBounds(bounds$i); \n";
-    
-}
-   
 ?>
