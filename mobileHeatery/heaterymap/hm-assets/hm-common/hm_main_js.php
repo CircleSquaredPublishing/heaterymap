6be6142a7b489092152afcc32e278be9ac2e6f42
Weer <circlesquaredpublishing@gmail.com>
2015-09-30 23:09:49 -0400

    javascript functions: mobile version

--- mobileHeatery/heaterymap/hm-assets/hm-common/hm_main_js.php
+++ mobileHeatery/heaterymap/hm-assets/hm-common/hm_main_js.php
@@ -1,52 +1,91 @@
 <script>
     var map;
     var heatmap;
+    var myData=[];
+    var TILE_SIZE = 256;
+    /*Begin Mercator Projection*/
+function bound(value, opt_min, opt_max) {
+	if (opt_min !== null) value = Math.max(value, opt_min);
+	if (opt_max !== null) value = Math.min(value, opt_max);
+	return value;
+}
+function degreesToRadians(deg) {
+	return deg * (Math.PI / 180);
+}
+function radiansToDegrees(rad) {
+	return rad / (Math.PI / 180);
+}
+function MercatorProjection() {
+	this.pixelOrigin_ = new google.maps.Point(TILE_SIZE / 2, TILE_SIZE / 2);
+	this.pixelsPerLonDegree_ = TILE_SIZE / 360;
+	this.pixelsPerLonRadian_ = TILE_SIZE / (2 * Math.PI);
+}
+MercatorProjection.prototype.fromLatLngToPoint = function(latLng, opt_point) {
+	var me = this;
+	var point = opt_point || new google.maps.Point(0, 0);
+	var origin = me.pixelOrigin_;
+	point.x = origin.x + latLng.lng() * me.pixelsPerLonDegree_;
+	var siny = bound(Math.sin(degreesToRadians(latLng.lat())), -0.9999, 0.9999);
+	point.y = origin.y + 0.5 * Math.log((1 + siny) / (1 - siny)) * -me.pixelsPerLonRadian_;
+	return point;
+};
+MercatorProjection.prototype.fromPointToLatLng = function(point) {
+	var me = this;
+	var origin = me.pixelOrigin_;
+	var lng = (point.x - origin.x) / me.pixelsPerLonDegree_;
+	var latRadians = (point.y - origin.y) / -me.pixelsPerLonRadian_;
+	var lat = radiansToDegrees(2 * Math.atan(Math.exp(latRadians)) - Math.PI / 2);
+	return new google.maps.LatLng(lat, lng);
+};
+var desiredRadiusPerPointInMeters = 300;
+function getNewRadius() {
+	var numTiles = 1 << map.getZoom();
+	var center = map.getCenter();
+	var moved = google.maps.geometry.spherical.computeOffset(center, 10000, 90); /*1000 meters to the right*/
+	var projection = new MercatorProjection();
+	var initCoord = projection.fromLatLngToPoint(center);
+	var endCoord = projection.fromLatLngToPoint(moved);
+	var initPoint = new google.maps.Point(
+		initCoord.x * numTiles,
+		initCoord.y * numTiles);
+	var endPoint = new google.maps.Point(
+		endCoord.x * numTiles,
+		endCoord.y * numTiles);
+	var pixelsPerMeter = (Math.abs(initPoint.x - endPoint.x)) / 10000.0;
+	var totalPixelSize = Math.floor(desiredRadiusPerPointInMeters * pixelsPerMeter);
+	console.log(totalPixelSize);
+	return totalPixelSize;
+}
+/*End Mercator Projection*/
     function displayMap() {
-        var retro_style = new google.maps.StyledMapType(retroStyle, {
-            name: "Retro"
-        });
-        var apple_style = new google.maps.StyledMapType(appleStyle, {
-            name: "Apple"
-        });
-        var light_style = new google.maps.StyledMapType(lightStyle, {
-            name: "Dusk"
-        });
-        var old_style = new google.maps.StyledMapType(oldStyle, {
-            name: "Vintage"
-        });
-        var pale_style = new google.maps.StyledMapType(paleStyle, {
-            name: "Cloud"
-        });
         var brown_style = new google.maps.StyledMapType(brownStyle, {
             name: "Organic"
         });
         var myOptions = {
             zoom: 12,
+            minZoom: 12,
             center: new google.maps.LatLng(<?php echo $latitude; ?>, <?php echo $longitude;?>),
+            scrollWheelControl: true,
             draggable: true,
             panControl: false,
             zoomControl: true,
             zoomControlOptions: {
-                position: google.maps.ControlPosition.TOP_LEFT,
+                position: google.maps.ControlPosition.TOP_RIGHT,
                 style: google.maps.ZoomControlStyle.SMALL
             },
-            mapTypeControl: true,
-            streetViewControl: false,
-            scrollWheelControl: true,
-            mapTypeControlOptions: {
-                position: google.maps.ControlPosition.RIGHT_TOP,
-                style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
-                mapTypeIds: ["Retro", "Apple", "Dusk", "Vintage", "Cloud", "Organic", google.maps.MapTypeId.ROADMAP, google.maps.MapTypeId.SATELLITE, google.maps.MapTypeId.TERRAIN]
-            }
+            mapTypeControl: false,
+            streetViewControl: false
+            
         };
         map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
-        map.mapTypes.set("Retro", retro_style);
-        map.mapTypes.set("Apple", apple_style);
-        map.mapTypes.set("Dusk", light_style);
-        map.mapTypes.set("Vintage", old_style);
-        map.mapTypes.set("Cloud", pale_style);
         map.mapTypes.set("Organic", brown_style);
-        map.setMapTypeId("Retro");
+        map.setMapTypeId("Organic");
+        
+google.maps.event.addListener(map, 'zoom_changed', function() {
+        heatmap.setOptions({
+        radius: getNewRadius()
+		});
+	});
 
 $(document).ready(function(){
     $.ajax({
@@ -69,7 +108,7 @@ $(document).ready(function(){
             }
             heatmap = new google.maps.visualization.HeatmapLayer({
                 data: myData,
-                radius: 15,
+                radius: getNewRadius(),
                 opacity: 0.3,
                 gradient: gradientNew,
                 map: map
@@ -82,14 +121,7 @@ $(document).ready(function(){
 <?php require ($common_path . 'hm_add.php');?>
 
 }
-    
-function changeRadius(r) {
-    heatmap.set('radius', r * 1);
-}
-function changeOpacity(o) {
-    heatmap.set('opacity', o);
-}
 
-    google.maps.event.addDomListener(window, 'load', displayMap);
+google.maps.event.addDomListener(window, 'load', displayMap);
     
 </script>
